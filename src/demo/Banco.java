e64edf49eff39353b9eb479200147e3f30ad6503
Vinicius <Vinicius@Vinicius-PC>
2020-08-13 19:39:02 -0300

    Novo Banco

---
+++ src/demo/Banco.java
@@ -0,0 +1,23 @@
+package demo;
+ class Banco {
+     public static void main (String[] args){
+     int conta1 = 1;
+     String nomeConta1 = "Vinicius";
+     float saldoConta1 = 200;
+     System.out.println("Conta: "+ conta1 + " - " + nomeConta1 + "(RS " + saldoConta1 + ")");
+     
+     int conta2 = 2;
+     String nomeConta2 = "Xunning";
+     float saldoConta2 = 500;
+     System.out.println("Conta: "+ conta2 + " - " + nomeConta2 + "(RS " + saldoConta2 + ")");
+     
+     saldoConta1 -= 100;
+     saldoConta2 += 100;
+     
+    System.out.println("Conta: "+ conta1 + " - " + nomeConta1 + "(RS " + saldoConta1 + ")");
+    System.out.println("Conta: "+ conta2 + " - " + nomeConta2 + "(RS " + saldoConta2 + ")");
+     
+     
+    
+}
+ }
